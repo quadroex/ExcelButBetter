091ae66f3352f0e5908d461da88eda62b06a26c9
Quadroex <zjdj572@gmail.com>
2025-11-26 09:45:42 +0200

    Update MauiProgram.cs

--- ExcelButBetter/MauiProgram.cs
+++ ExcelButBetter/MauiProgram.cs
@@ -57,30 +57,3 @@ namespace ExcelButBetter
         }
     }
 }
-
-//using Microsoft.Extensions.Logging;
-//using CommunityToolkit.Maui; // Додати цей using
-
-//namespace ExcelButBetter
-//{
-//    public static class MauiProgram
-//    {
-//        public static MauiApp CreateMauiApp()
-//        {
-//            var builder = MauiApp.CreateBuilder();
-//            builder
-//               .UseMauiApp<App>()
-//               .UseMauiCommunityToolkit() // <--- ДОДАТИ ЦЕЙ РЯДОК
-//               .ConfigureFonts(fonts =>
-//               {
-//                   fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-//                   fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-//               });
-
-//#if DEBUG
-//            builder.Logging.AddDebug();
-//#endif
-//            return builder.Build();
-//        }
-//    }
-//}
